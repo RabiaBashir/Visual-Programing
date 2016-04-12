2357662f9e831b93840f3c24bc2090bc702fb69b
Rabia Bashir <rabia.bashir11012@gmail.com>
2016-04-13 01:52:47 +0500

    Namespace Example

---
+++ namespace_1/namespace_1/Program.cs
@@ -0,0 +1,42 @@
+﻿//Namespace Example
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace namespace_1
+{
+    
+        class namespace_cl
+        {
+            public void func()
+            {
+                Console.WriteLine("First");
+            }
+        }
+    }
+
+    namespace second_space
+    {
+        class namespace_cl
+        {
+            public void func()
+            {
+                Console.WriteLine("Second");
+            }
+        }
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            namespace_1.namespace_cl fc = new namespace_1.namespace_cl();
+            second_space.namespace_cl sc = new second_space.namespace_cl();
+            fc.func();
+            sc.func();
+            Console.ReadKey();
+        }
+    }
