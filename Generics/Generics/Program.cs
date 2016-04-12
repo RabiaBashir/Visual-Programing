f9fa6ca7ffd6c6b0f1c4456d42bf1413b1566b83
Rabia Bashir <rabia.bashir11012@gmail.com>
2016-04-13 01:43:46 +0500

    Generics Example

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            char c, d;
+            a = 10;
+            b = 20;
+            c = 'R';
+            d = 'F';
+
+            //display values before swap:
+            Console.WriteLine("Integer values before calling swap:");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("Character values before calling swap:");
+            Console.WriteLine("c = {0}, d = {1}", c, d);
+
+            //call swap
+            Swap<int>(ref a, ref b);
+            Swap<char>(ref c, ref d);
+
+            //display values after swap:
+            Console.WriteLine("Integer values after calling swap:");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("Character values after calling swap:");
+            Console.WriteLine("c = {0}, d = {1}", c, d);
+
+            Console.ReadKey();
+        }
+    }
+}
+    
+
