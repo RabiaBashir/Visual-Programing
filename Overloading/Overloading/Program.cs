2a86b0b896ce986a1b283d0f334943a14cdd01aa
Rabia Bashir <rabia.bashir11012@gmail.com>
2016-04-13 00:53:47 +0500

    Overloading Concept

---
+++ Overloading/Overloading/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Program
+    {
+        
+        protected int Cgpa;
+        public virtual int gpa()
+        {
+            Console.WriteLine("parent");
+            Console.ReadLine();
+            return 0;
+        }
+        class bse : Program
+        {
+           
+            public override int gpa()
+            {
+                Console.WriteLine("child");
+                Console.ReadLine();
+                return 0;
+            }
+            class Caller
+            {
+                public void CallGpa(Program sh)
+                {
+                    int a;
+                    a = sh.gpa();
+                    Console.WriteLine("", a);
+                }
+            }  
+
+            static void Main(string[] args)
+            {
+                Caller c = new Caller();
+                
+                bse b = new bse();
+                
+
+                c.CallGpa(b);
+            }
+            
+        }
+    }
+}
+
