242f79ffa1583dbd3c7b36388a881a91303c7047
Rabia Bashir <rabia.bashir11012@gmail.com>
2016-04-13 01:17:13 +0500

    DELEGATE CONCEPT

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,45 @@
+﻿//DELEGATE EXAMPLE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+delegate int NumberChanger(int n);
+
+namespace Delegate
+{
+    class Program
+    {
+        static int num = 100;
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            //create delegate instances
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultNum);
+
+            //calling the methods using the delegate objects
+            nc1(5);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            nc2(50);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
+        }
+    }
+}
