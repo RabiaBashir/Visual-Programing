15bde7351f26d12402851ccdb74efcd6b87077ec
Rabia Bashir <rabia.bashir11012@gmail.com>
2016-04-13 01:09:47 +0500

    Sorting in string Example

---
+++ Sorting in String/Sorting in String/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Sorting_in_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                string name1 = "RABIA BASHIR";
+                string name2 = "FARAH KHAN";
+
+                // Get position of space character.
+                int index1 = name1.IndexOf(" ");
+                index1 = index1 < 0 ? 0 : index1--;
+                Console.WriteLine(index1);
+                int index2 = name2.IndexOf(" ");
+                index2 = index1 < 0 ? 0 : index2--;
+                Console.WriteLine(index2);
+                int length = Math.Max(name1.Length, name2.Length);
+                Console.WriteLine(length);
+
+                Console.WriteLine("Sorted alphabetically by last name:");
+                if (String.Compare(name1, index1, name2, index2, length,
+                                   new CultureInfo("en-US"), CompareOptions.IgnoreCase) < 0)
+                    Console.WriteLine("{0}\n{1}", name1, name2);
+                else
+                    Console.WriteLine("{0}\n{1}", name2, name1); 
+
+    Console.ReadLine();
+
+    }
+}
+
+}
+        
