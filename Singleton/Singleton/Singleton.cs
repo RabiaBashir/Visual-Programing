b759d10a42546129397f93a6ed2a608c113dabb1
Rabia Bashir <rabia.bashir11012@gmail.com>
2016-05-08 21:33:42 +0500

    Singleton example

---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+  public  class Singleton
+    {
+      private static Singleton obj;
+      private Singleton()
+      {
+
+      }
+      public static Singleton OBJ
+      {
+          get
+          {
+                if(obj==null)
+                {
+                    obj = new Singleton();
+                }
+                return obj;
+                Console.WriteLine(obj);
+          }
+          
+      }
+     
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.obj;
+            Singleton s2 = Singleton.obj;
+
+            
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+             Console.ReadLine();
+        }
+    }
+}
